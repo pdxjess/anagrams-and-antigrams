6a9a188419cfb5f98b41db70281808e429193444
Jessica Doss <doss.jessicam@gmail.com>
2019-07-19 10:47:52 -0700

    fail first test

--- lib/anaanti_logic.rb
+++ lib/anaanti_logic.rb
@@ -1,1 +1,14 @@
 require('pry')
+
+  class Anagram
+    def initialize(word)
+      @word = word
+    end
+    def compared_words(word1, word2)
+      @word = word1
+      if word1.chars.sort() == word2.chars.sort()
+        "These are anagrams!"
+      else word1.chars.sort() != word2.chars.sort()
+        "These are antigrams!"
+    end
+  end
--- spec/anaanti_logic_spec.rb
+++ spec/anaanti_logic_spec.rb
@@ -1,2 +1,10 @@
 require('rspec')
 require('pry')
+require('anaanti_logic')
+
+describe('#Anagram') do
+  it("returns true if 'cat' matches 'tac'") do
+    words = Anagram.new()
+    expect(words.compared_words("ruby", "cat")).to(eq("These are anagrams!"))
+  end
+end
