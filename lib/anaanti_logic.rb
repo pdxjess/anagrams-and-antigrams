6b1393e37e467146a83e1fde98bc7c8e0a1a86a6
Jessica Doss <doss.jessicam@gmail.com>
2019-07-19 16:09:33 -0700

    fix typing error and add last else

--- lib/anaanti_logic.rb
+++ lib/anaanti_logic.rb
@@ -2,20 +2,22 @@ require('pry')
 
   class Anagram
     def initialize(word)
-      @word = word
+      @word1 = word1
     end
 
     def compared_words(word1, word2)
-      @word = word1.downcase
+      @word1 = word1.downcase
       word1 = word1.gsub(/[.!'?,]/, '')
       word2 = word2.gsub(/[.!'?,]/, '')
       if word1.chars.sort() == word2.chars.sort()
         "These are anagrams!"
       elsif word1.scan(/[aeiouy]/).count == 0
         "Invalid word"
-      elisf word2.scan(/[aeiouy]/).count == 0
+      elsif word2.scan(/[aeiouy]/).count == 0
         "Invalid word"
-      else word1.chars.sort() != word2.chars.sort()
+      elsif word1.chars.sort() != word2.chars.sort()
         "These are antigrams!"
+      else
+        "Enter two words:"
     end
   end
