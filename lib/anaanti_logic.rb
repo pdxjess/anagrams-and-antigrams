6533a4e5e9e32515ff6550615e3dfbbe1d163735
Jessica Doss <doss.jessicam@gmail.com>
2019-07-21 22:20:11 -0700

    added/fixed downcase rule

--- lib/anaanti_logic.rb
+++ lib/anaanti_logic.rb
@@ -6,19 +6,21 @@ require('pry')
     end
 
     def compared_words(word1, word2)
-      @word1 = word1.downcase
-      word2 = word2.downcase
+      @word1 = word1
       word1 = word1.gsub(/[.!'?,]/, '')
       word2 = word2.gsub(/[.!'?,]/, '')
       if word1.chars.sort() == word2.chars.sort()
-        "These are anagrams!"
+        "These are anagrams"
+      elsif word1.downcase() == word2.downcase()
+        "These are anagrams"
       elsif word1.scan(/[aeiouy]/).count == 0
         "Invalid word"
       elsif word2.scan(/[aeiouy]/).count == 0
         "Invalid word"
       elsif word1.chars.sort() != word2.chars.sort()
-        "These are antigrams!"
+        "These are antigrams"
       else
         "Enter two words:"
     end
   end
+end
--- spec/anaanti_logic_spec.rb
+++ spec/anaanti_logic_spec.rb
@@ -4,22 +4,23 @@ require('anaanti_logic')
 
 describe('#Anagram') do
   it("returns true if 'cat' matches 'tac'") do
-    words = Anagram.new()
+    word1 = Anagram.new()
     expect(words.compared_words("tac", "cat")).to(eq("These are anagrams!"))
   end
   it("returns true even if letters are capitalized 'CAt' matches 'Tac'") do
-    words = Anagram.new()
+    word1 = Anagram.new()
     expect(words.compared_words("CAt", "Tac")).to(eq("These are anagrams!"))
   end
   it("returns false when the letters do not match") do
-    words = Anagram.new()
+    word1 = Anagram.new()
     expect(words.compared_words("Jess", "George")).to(eq("These are antigrams!"))
   end
   it("returns false when the letters do not match") do
-    words = Anagram.new()
+    word1 = Anagram.new()
     expect(words.compared_words("Cat's", "Tac.s")).to(eq("These are anagrams!"))
   end
   it("returns false telling a user that it is not a word") do
-    words = Anagram.new()
+    word1 = Anagram.new()
     expect(words.compared_words("wpg", "mdl")).to(eq("Invalid word!"))
+  end
 end
