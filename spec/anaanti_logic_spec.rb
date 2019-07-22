785627a5b5f8caaa0e20d25e434a72543b97b527
Jessica Doss <doss.jessicam@gmail.com>
2019-07-21 22:35:10 -0700

    clean up repetitiveness in tests

--- spec/anaanti_logic_spec.rb
+++ spec/anaanti_logic_spec.rb
@@ -4,23 +4,19 @@ require('anaanti_logic')
 
 describe('#Anagram') do
   it("returns true if 'cat' matches 'tac'") do
-    word1 = Anagram.new()
-    expect(word1.compared_words("tac", "cat")).to(eq("These are anagrams!"))
+    word1 = Anagram.new('tac')
+    expect(word1.compared_words?('cat')).to(eq(true))
   end
   it("returns true even if letters are capitalized 'CAt' matches 'Tac'") do
-    word1 = Anagram.new()
-    expect(word1.compared_words("CAt", "Tac")).to(eq("These are anagrams!"))
+    expect(word1.compared_words?("Tac")).to(eq("These are anagrams!"))
   end
   it("returns false when the letters do not match") do
-    word1 = Anagram.new()
-    expect(word1.compared_words("Jess", "George")).to(eq("These are antigrams!"))
+    expect(word1.compared_words?("loop")).to(eq("These are antigrams!"))
   end
-  it("returns false when the letters do not match") do
-    word1 = Anagram.new()
-    expect(word1.compared_words("Cat's", "Tac.s")).to(eq("These are anagrams!"))
+  it("returns true even when their are apostrophes or periods") do
+  expect(word1.compared_words?("Tac.s")).to(eq("These are anagrams!"))
   end
   it("returns false telling a user that it is not a word") do
-    word1 = Anagram.new()
-    expect(word1.compared_words("wpg", "mdl")).to(eq("Invalid word!"))
+    expect(word1.compared_words?("mdl")).to(eq("Invalid word!"))
   end
 end
